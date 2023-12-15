523813d3f67de2384bcbb4ca06113e9bfa2892d4
TRANVANSANG11 <153897127+TRANVANSANG11@users.noreply.github.com>
2023-12-15 16:31:51 +0700

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,42 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Thongtinmotsinhvien
+		String tenSV = "Tran Van Sang";
+		int maSV = 1160224;
+		long cccd = 01235545522;
+		String className = "23CDTH41";
+		String schoolName = "Hueic";
+		float dtb = 5.0f;
+		// Thongtincongnhan
+		String tenCN = "Van Sang";
+		String diachi = " 140 Trưng Nữ Vương";
+		long sdt = 123456789;
+		float hesoluong = 5000000f;
+		// thongtinmotchiecxecar
+		String tenxe = "ban gioi han";
+		String hang = "yamaha";
+		long gia = 1234567891;
+		String mauson = "Do";
+		// TTSV
+		System.out.println("Ho va ten:" + tenSV);
+		System.out.println("Ma SV:" + maSV);
+		System.out.println("Can cuoc cong dan:" + cccd);
+		System.out.println("Ten Lop:" + className);
+		System.out.println("Ten Truong:" + schoolName);
+		System.out.println("Diem trung binh:" + dtb);
+		// TTCN
+		System.out.println("Ten Cong Nhan:" + tenCN);
+		System.out.println("Dia chi:" + diachi);
+		System.out.println("So dien thoai:" + sdt);
+		System.out.println("He so luong:" + hesoluong);
+		// TTX
+		System.out.println("Ten Xe:" + tenxe);
+		System.out.println("Hang:" + hang);
+		System.out.println("Gia:" + gia);
+		System.out.println("Mau son:" + mauson);
+	}
+}
